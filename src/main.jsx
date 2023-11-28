85d9e49deca60dbfc722770ff2b0c0053d0e26b2
Joel Lozano <joel.lozano@rappi.com>
2023-11-28 17:21:23 -0600

    aside and fix margins

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './components/navBar/NavBar.jsx'
+import Aside from './components/aside/Aside.jsx'
 import Items from './pages/items/Items.jsx'
 import './index.scss'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
-    <Items />
+    <div className='main-content'>
+      <Aside className="main-aside"/>
+      <Items className="main-items" />
+    </div>
   </React.StrictMode>,
 )
