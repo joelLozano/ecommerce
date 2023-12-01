502b0c2b7de2ba0e107cd096b287804db79e9671
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-11-30 20:09:35 -0600

    useForm hook listo

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,11 +1,29 @@
-
+import useForm from "../../hooks/useForm"
 
 export default function Signup() {
+  // Paso 1: Crear un objeto con los valores iniciales 
+  const datosPorDefecto = {
+    first_name: '',
+    last_name: '',
+    gender: 'F',
+    email: '',
+    password: '',
+    role: ''
+  }
+
+  // Paso 2: Crear una función que se va a ejecutar cuando haga click en botón "Regístrate"
+  const enviarDatos = (datos) => {
+    console.log('Datos enviados ', datos)
+  }
+
+  // Paso 3: Usar mi custom hook de useForm (primero importarlo)
+  const { formData, handleInputChange, handleSubmit } = useForm(datosPorDefecto, enviarDatos) 
+
   return (
     <div>
       <div className="form-container">
         <h1 className="form-container__title">Sign Up.</h1>
-        <form onSubmit={()=>{}} className="form">
+        <form onSubmit={handleSubmit} className="form">
           <div className="form__form-group">
             <label htmlFor='first_name' className="form__form-group__label">Nombre</label>
             <input
@@ -13,8 +31,8 @@ export default function Signup() {
               name='first_name'
               placeholder='Tu nombre'
               id='first_name'
-              value=''
-              onChange={()=>{}}
+              value={formData.first_name}
+              onChange={handleInputChange}
               className="form__form-group__input"
             />
           </div>
@@ -27,8 +45,8 @@ export default function Signup() {
               name='last_name'
               placeholder='Tu apellido'
               id='last_name'
-              value=''
-              onChange={()=>{}}
+              value={formData.last_name}
+              onChange={handleInputChange}
               className="form__form-group__input"
             />
           </div>
@@ -41,8 +59,8 @@ export default function Signup() {
               name='email'
               placeholder='correo@mail.com'
               id='email'
-              value=''
-              onChange={()=>{}}
+              value={formData.email}
+              onChange={handleInputChange}
               className="form__form-group__input"
             />
           </div>
@@ -54,16 +72,16 @@ export default function Signup() {
               type='password'
               name='password'
               id='password'
-              value=''
-              onChange={()=>{}}
+              value={formData.password}
+              onChange={handleInputChange}
               className="form__form-group__input"
             />
           </div>
 
 
           <div className="form__form-group">
-            <label htmlFor='role' className="form__form-group__label">Role</label>
-            <select name='role' id='role' value='' onChange={()=>{}}>
+            <label htmlFor='role' className="form__form-group__label">Rol</label>
+            <select name='role' id='role' value={formData.role} onChange={handleInputChange}>
               <option value=''>Elige un tipo de usuario</option>
               <option value='CUSTOMER'>Cliente</option>
               <option value='ADMIN'>Administrador</option>
